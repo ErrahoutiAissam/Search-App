fa1f90735852bc45ca216a9c83bc3d9efae38ca3
ErrahoutiAissam <milbournato@gmail.com>
2022-08-23 16:07:39 +0100

    fixed a bug

--- js/dataFunctions.js
+++ js/dataFunctions.js
@@ -1,4 +1,4 @@
-export const getSearchTerm = () =>{
+export const getSearchTerm = () => {
     const dummySearchTerm = document.getElementById("search").value.trim();
     const regex = /[ ]{2,}/gi;
     const searchTerm = dummySearchTerm.replaceAll(regex, " ");
@@ -6,25 +6,26 @@ export const getSearchTerm = () =>{
 
 }
 
-export const retreiveSearchResults = async (searchTerm) =>{
+export const retreiveSearchResults = async (searchTerm) => {
     const wikiSearchText = getWikiSearchText(searchTerm);
     const wikiSearchResults = await fetchData(wikiSearchText);
     let resultArray = [];
-    if (wikiSearchResults.hasOwnProperty("query")){
+    if (wikiSearchResults.hasOwnProperty("query")) {
         resultArray = handlingWikiResults(wikiSearchResults.query.pages);
     }
     return resultArray;
-   
+
 }
 
-const getWikiSearchText = (searchTerm) =>{
+const getWikiSearchText = (searchTerm) => {
     const maxChars = getCharsMax();
     const dummySearchText = `https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=${searchTerm}&gsrlimit=20&prop=pageimages|extracts&exchars=${maxChars}&exintro&explaintext&exlimit=max&format=json&origin=*`;
     const searchText = encodeURI(dummySearchText);
+    //for the spaces and special chars  
     return searchText;
 }
 
-const getCharsMax = () =>{
+const getCharsMax = () => {
     const width = window.innerWidth || document.body.clientWidth;
     let maxChars;
     if (width < 414) maxChars = 65;
@@ -33,12 +34,12 @@ const getCharsMax = () =>{
     return maxChars;
 }
 
-const fetchData = async (searchTerm) =>{
-    try{
+const fetchData = async (searchTerm) => {
+    try {
         const response = await fetch(searchTerm);
         const data = await response.json();
         return data;
-    } catch(err){
+    } catch (err) {
         console.error(err);
     }
 }
@@ -49,15 +50,15 @@ const handlingWikiResults = (results) => {
         const id = key;
         const title = results[key].title;
         const text = results[key].extract;
-        const img  = results[key].hasOwnProperty("thumbnail")
+        const img = results[key].hasOwnProperty("thumbnail")
             ? results[key].thumbnail.source : null;
-            const item = {
-                id :  id,
-                title : title,
-                img: img,
-                text: text   
-            };
-            arrayResult.push(item);
+        const item = {
+            id: id,
+            title: title,
+            img: img,
+            text: text
+        };
+        arrayResult.push(item);
     });
     return arrayResult;
 }
